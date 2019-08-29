aadfc2f255449197b708248fb27f862a3eb26046
Dominik Kuss <dominik@codebreak.de>
2019-08-29 23:53:12 +0200

    - removed trip summaries for trips under 100m - removed waiting log entries except for 0 and 20 to reduce spam

--- logfile2Telegram/logfile2Telegram.php
+++ logfile2Telegram/logfile2Telegram.php
@@ -1,7 +1,7 @@
 <?php
 	require_once 'logfile2Telegram.config.php';
 	
-	$input  = shell_exec('tail -n5000 /etc/teslalogger/nohup.out');
+	$input  = shell_exec('tail -n10000 /etc/teslalogger/nohup.out');
 	if ($input === false) {
         // Handle the error
 		echo "error";
@@ -33,7 +33,14 @@
     	}
     	if (strpos($line, 'Streamingtoken:') !== false){
     		continue;
-    	}
+		}
+		if (strpos($line, 'Waiting for car to go to sleep') !== false){
+			$numberPos = strrpos($line, ' ') + 1;
+			$number = (int) trim(substr($line, $numberPos));
+			if (!in_array($number, [0,20])) {
+				continue;
+			}
+		}
 
     	//16.08.2019 20:44:46	
     	$dateTimeString = substr ($line ,  0 , 19 ); 
@@ -60,12 +67,57 @@
 		$telegram_message = $telegram_message . $line;
 	}
 
+    if (strpos($telegram_message, 'StopStreaming') !== false){
+		if ($trip = getTripOutput()) {
+			$telegram_message = $telegram_message . $trip;
+		}
+    }
+
 	echo nl2br($telegram_message);
 
 	$data = [
 	    'text' => $telegram_message,
 	    'chat_id' => CHAT_ID
 	];
+
 	if (sizeof($outputArray) > 0){
 		echo file_get_contents("https://api.telegram.org/". BOT_IDENTIFIER ."/sendMessage?" . http_build_query($data) );
 	}
+
+
+function getTripOutput(){
+
+    $input = file_get_contents('/etc/teslalogger/current_json.txt');
+    if ($input === false) {
+       // Handle the error
+       // echo 'error reading file';
+       return false;
+    }
+
+    $input = removeBOM($input);
+	$data = json_decode($input);
+	
+	// trips under 100m are not interesting
+	if ($data->trip_distance < 0.1) {
+		return false;
+	}
+
+    $output = "------------------------\n";
+    $output = $output . "Batteriestand: " .$data->battery_level . "%\n";
+    $output = $output . "Ideale Reichweite: " .number_format($data->ideal_battery_range_km, 2, "," , "." ) . " km\n";
+    $output = $output . "Kilometerstand: " .number_format($data->odometer, 0, "," , "." ) . " km\n";
+    $output = $output . "Letzter Trip: \n";
+    $output = $output . "Start: " .$data->trip_start . "\n";
+    $output = $output . "Dauer: " .number_format($data->trip_duration_sec/60, 0, "," , "." ) . " Minuten\n";
+    $output = $output . "Verbrauch: " .number_format($data->trip_kwh, 2, "," , "." ) . " kwh\n";
+    $output = $output . "Durchschnittsverbrauch: " .number_format($data->trip_avg_kwh, 2, "," , "." ) . " kwh\n";
+    $output = $output . "Entfernung: " .number_format($data->trip_distance, 2, "," , "." ) . " km\n";
+    return $output;
+}
+
+function removeBOM($data) {
+    if (0 === strpos(bin2hex($data), 'efbbbf')) {
+        return substr($data, 3);
+    }
+    return $data;
+}
